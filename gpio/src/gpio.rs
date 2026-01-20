5920e272f7b107b7def4dd1f31fda9d88a8bf61c
Jiaye Zou <tony200303.zt@gmail.com>
2026-01-19 23:31:44 -0800

    gpio done

--- gpio/src/gpio.rs
+++ gpio/src/gpio.rs
@@ -1,3 +1,6 @@
+use core::pin::Pin;
+use crab_pi::println;
+
 const GPIO_BASE: *mut u32 = ::core::ptr::with_exposed_provenance_mut(0x2020_0000);
 const GPIO_SET0_OFFSET: u32 = 0x1c;
 const GPIO_CLR0_OFFSET: u32 = 0x28;
@@ -25,6 +28,25 @@ pub fn gpio_set_output(pin: u32) {
     }
 }
 
+pub fn gpio_set_input(pin: u32) {
+    if (pin > GPIO_MAX_PIN) {
+        panic!("Invalid GPIO pin number");
+    }
+
+    let fsel_reg_num = pin / 10;
+    let fsel_offset = 3 * (pin % 10);
+
+    let fsel_reg = unsafe {
+        GPIO_BASE.byte_offset((fsel_reg_num * 4) as isize)
+    };
+
+    unsafe {
+        let mut cur_val = *fsel_reg;
+        cur_val &= !(7 << fsel_offset);
+        *fsel_reg = cur_val;
+    }
+}
+
 pub fn gpio_set_on(mut pin: u32) {
     if (pin > GPIO_MAX_PIN) {
         panic!("Invalid GPIO pin number");
@@ -36,7 +58,7 @@ pub fn gpio_set_on(mut pin: u32) {
 
     if (pin >= 32) {
         gpio_set = unsafe {
-            GPIO_BASE.byte_offset(4)
+            gpio_set.byte_offset(4)
         };
 
         pin -= 32;
@@ -52,19 +74,50 @@ pub fn gpio_set_off(mut pin: u32) {
         panic!("Invalid GPIO pin number");
     }
 
-    let mut gpio_set = unsafe {
+    let mut gpio_clr = unsafe {
         GPIO_BASE.byte_offset(GPIO_CLR0_OFFSET as isize)
     };
 
     if (pin >= 32) {
-        gpio_set = unsafe {
-            GPIO_BASE.byte_offset(4)
+        gpio_clr = unsafe {
+            gpio_clr.byte_offset(4)
         };
 
         pin -= 32;
     }
 
     unsafe {
-        *gpio_set = 1 << pin;
+        *gpio_clr = 1 << pin;
+    }
+}
+
+pub fn gpio_write(pin: u32, value: bool) {
+    if (value) {
+        gpio_set_on(pin);
+    } else {
+        gpio_set_off(pin);
+    }
+}
+
+pub fn gpio_read(mut pin: u32) -> bool {
+    if (pin > GPIO_MAX_PIN) {
+        panic!("Invalid GPIO pin number");
+    }
+
+    let mut gpio_lev = unsafe {
+        GPIO_BASE.byte_offset(GPIO_LEV0_OFFSET as isize)
+    };
+
+    if (pin >= 32) {
+        gpio_lev = unsafe {
+            gpio_lev.byte_offset(4)
+        };
+
+        pin -= 32;
+    }
+
+    unsafe {
+        let lev_val = *gpio_lev;
+        return (lev_val & (1 << pin)) != 0;
     }
 }
--- gpio/src/main.rs
+++ gpio/src/main.rs
@@ -8,16 +8,87 @@ use crab_pi::println;
 
 use gpio::*;
 
-#[unsafe(no_mangle)]
-fn __user_main() {
-    println!("Hello, world!");
+const LED_1: u32 = 20;
+const LED_2: u32 = 27;
+const ACT: u32 = 47;
+const OUTPUT: u32 = 9;
+const INPUT: u32 = 8;
 
-    gpio_set_output(47);
+fn _1_blink() {
+    gpio_set_output(LED_1);
 
     loop {
-        gpio_set_on(47);
+        gpio_set_on(LED_1);
         delay_cycles(1000000);
-        gpio_set_off(47);
+        gpio_set_off(LED_1);
         delay_cycles(1000000);
     }
 }
+
+fn _2_blink() {
+    gpio_set_output(LED_1);
+    gpio_set_output(LED_2);
+
+    loop {
+        gpio_set_on(LED_1);
+        gpio_set_off(LED_2);
+        delay_cycles(3000000);
+        gpio_set_off(LED_1);
+        gpio_set_on(LED_2);
+        delay_cycles(3000000);
+    }
+}
+
+fn _3_loopback() {
+    gpio_set_output(LED_1);
+    gpio_set_output(LED_2);
+    gpio_set_input(INPUT);
+    gpio_set_output(OUTPUT);
+    
+    let mut v = false;
+    
+    loop {
+        gpio_write(LED_1, v);
+        
+        gpio_write(OUTPUT, v);
+        gpio_write(LED_2, gpio_read(INPUT));
+        
+        delay_cycles(1000000);
+        
+        v = !v;
+    }
+}
+
+fn _4_act_blink() {
+    gpio_set_output(ACT);
+
+    loop {
+        gpio_set_on(ACT);
+        delay_cycles(1000000);
+        gpio_set_off(ACT);
+        delay_cycles(1000000);
+    }
+}
+
+fn _5_all() {
+    gpio_set_output(LED_1);
+    gpio_set_output(LED_2);
+    gpio_set_output(ACT);
+
+    loop {
+        gpio_set_off(ACT);
+        gpio_set_on(LED_1);
+        gpio_set_on(LED_2);
+        delay_cycles(1000000);
+        gpio_set_on(ACT);
+        gpio_set_off(LED_1);
+        gpio_set_off(LED_2);
+        delay_cycles(1000000);
+    }
+}
+
+#[unsafe(no_mangle)]
+fn __user_main() {
+    _3_loopback();
+}
+
